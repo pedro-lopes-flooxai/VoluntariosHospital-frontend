ded1177900327f1796eec2302ca47b69966de20e
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-20 17:01:02 -0300

    feat: add home

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Tela Inicial">
+        <div className="display-4">Conteúdo</div>
+        <hr />
+        <p className="mb-0">Paragráfo</p>
+    </Main>
