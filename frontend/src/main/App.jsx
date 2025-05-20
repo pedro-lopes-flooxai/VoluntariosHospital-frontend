4e20771575fd7283397552a3ec8f96dd2486abdc
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-20 16:59:43 -0300

    feat: add App

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import Logo from '../components/template/Logo'
+import Nav from '../components/template/Nav'
+import Routes from './Routes'
+
+
+export default props =>
+    <BrowserRouter>
+        <div className="app">
+            <Logo></Logo>
+            <Nav></Nav>
+            <Routes></Routes>
+        </div>
+    </BrowserRouter>
