33f6f86d1b9a16713cc69983fcc27205695711ea
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-20 17:01:37 -0300

    feat: add nav

---
+++ frontend/src/components/template/Nav.jsx
@@ -0,0 +1,21 @@
+import './Nav.css'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default props => 
+    <aside className="menu-area">
+        <nav className="menu">
+            <Link to="/">
+                <i className="fa fa-home"></i> Início
+            </Link>
+            <Link to="/users">
+                <i className="users"></i> Usuários
+            </Link>
+            <Link to="/tasks">
+                <i className="tasks"></i> Vagas
+            </Link>
+            <Link to="/leaderboard">
+                <i className="board"></i> Pontuação Geral
+            </Link>
+        </nav>
+    </aside>
