8a301823cc285a6dd8eda876c8dc5199176265d1
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-21 16:55:59 -0300

    feat: add new routes and nav

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -2,7 +2,7 @@ import './Nav.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default props => 
+export default props =>
     <aside className="menu-area">
         <nav className="menu">
             <Link to="/">
@@ -11,6 +11,10 @@ export default props =>
             <Link to="/users">
                 <i className="users"></i> Usuários
             </Link>
+            <Link to="/register">
+                <i className="register"></i> Cadastro de Usuários
+            </Link>
+
             <Link to="/tasks">
                 <i className="tasks"></i> Vagas
             </Link>
--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -4,14 +4,20 @@ import { Switch, Route, Redirect } from 'react-router'
 import Home from '../components/home/Home'
 import UserCrud from '../components/user/UserCrud'
 import Tasks from '../components/tasks/Tasks'
+import TasksDetails from '../components/tasks/TasksDetails';
 import Board from '../components/leaderboard/Board'
+import UserRegister from '../components/user/UserRegister';
+
 
 
 export default props =>
     < Switch >
         <Route exact path='/' component={Home} />
         <Route path='/users' component={UserCrud} />
+        <Route path='/register' component={UserRegister} />
+        <Route path='/tasks/:id' component={TasksDetails} />
         <Route path='/tasks' component={Tasks} />
         <Route path='/leaderboard' component={Board} />
+       
         <Redirect from='*' to='/' />
     </Switch >
