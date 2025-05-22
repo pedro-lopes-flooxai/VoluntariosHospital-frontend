078fa8b5d0a87fe61bcea23d5f879cce215c331e
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-22 16:39:20 -0300

    feat: add profile

---
+++ frontend/src/components/profile/Profile.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import Main from "../template/Main";
+import { Link } from "react-router-dom";
+import './Profile.css';
+
+const headerProps = {
+  icon: "users",
+  title: "Meu perfil",
+  subtitle: ""
+};
+
+export default class Profile extends Component {
+  render() {
+    return (
+      <Main {...headerProps}>
+        <div className="profile-container">
+          <div className="profile-card">
+            <div className="profile-section">
+              <p className="mb-0"><strong>Nome:</strong> </p>
+              <p className="mb-0"><strong>Email:</strong> </p>
+              <p className="mb-0"><strong>Pontos:</strong> </p>
+            </div>
+
+            <hr />
+
+            <div className="profile-section">
+              <Link to="/mytasks" className="profile-link">
+                Ver Tarefas Aceitas
+              </Link>
+              
+            </div>
+
+            <hr />
+
+            <div className="profile-section">
+              <Link to="/" className="profile-link highlight-exit">
+                Sair
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Main>
+    );
+  }
+}
