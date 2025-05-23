a3ff88e5c6ecd4a397da98cd8e79099200ae7e0c
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-23 16:52:03 -0300

    feat: add new route

--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-import Home from '../components/home/Home'
-import UserCrud from '../components/user/UserCrud'
-import Tasks from '../components/tasks/Tasks'
+import LandingPage from '../components/landingPage/LandingPage';
+import Home from '../components/home/Home';
+import UserCrud from '../components/user/UserCrud';
+import Tasks from '../components/tasks/Tasks';
 import TasksDetails from '../components/tasks/TasksDetails';
-import Board from '../components/leaderboard/Board'
+import Board from '../components/leaderboard/Board';
 import UserRegister from '../components/user/UserRegister';
-import Profile from '../components/profile/Profile'
+import Profile from '../components/profile/Profile';
 import MyTasks from '../components/profile/Mytasks';
 
-
-
-export default props =>
-    < Switch >
-        <Route exact path='/' component={Home} />
-        <Route path='/users' component={UserCrud} />
-        <Route path='/register' component={UserRegister} />
-        <Route path='/tasks/:id' component={TasksDetails} />
-        <Route path='/tasks' component={Tasks} />
-        <Route path='/leaderboard' component={Board} />
-        <Route path='/profile' component={Profile} />
-        <Route path='/mytasks' component={MyTasks} />
-
-
-        <Redirect from='*' to='/' />
-    </Switch >
+export default function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/users" element={<UserCrud />} />
+            <Route path="/register" element={<UserRegister />} />
+            <Route path="/tasks/:id" element={<TasksDetails />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/leaderboard" element={<Board />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/mytasks" element={<MyTasks />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+    );
+}
