26a887da3750456e6cea259d9643b5be6a927f0a
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-23 16:50:10 -0300

    refactor: update profile

--- frontend/src/components/profile/Profile.jsx
+++ frontend/src/components/profile/Profile.jsx
@@ -1,45 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 import Main from "../template/Main";
 import { Link } from "react-router-dom";
 import './Profile.css';
 
 const headerProps = {
-  icon: "users",
+  icon: "user",
   title: "Meu perfil",
   subtitle: ""
 };
 
-export default class Profile extends Component {
-  render() {
-    return (
-      <Main {...headerProps}>
-        <div className="profile-container">
-          <div className="profile-card">
-            <div className="profile-section">
-              <p className="mb-0"><strong>Nome:</strong> </p>
-              <p className="mb-0"><strong>Email:</strong> </p>
-              <p className="mb-0"><strong>Pontos:</strong> </p>
-            </div>
+const Profile = () => {
+  return (
+    <Main {...headerProps}>
+      <div className="profile-container">
+        <div className="profile-card">
+          <div className="profile-section">
+            <p className="mb-0"><strong>Nome:</strong> .</p>
+            <p className="mb-0"><strong>Email:</strong> .</p>
+            <p className="mb-0"><strong>Pontos:</strong> .</p>
+          </div>
 
-            <hr />
+          <hr />
 
-            <div className="profile-section">
-              <Link to="/mytasks" className="profile-link">
-                Ver Tarefas Aceitas
-              </Link>
-              
-            </div>
+          <div className="profile-section">
+            <Link to="/mytasks" className="profile-link">
+              Ver Tarefas Aceitas
+            </Link>
+          </div>
 
-            <hr />
+          <hr />
 
-            <div className="profile-section">
-              <Link to="/" className="profile-link highlight-exit">
-                Sair
-              </Link>
-            </div>
+          <div className="profile-section">
+            <Link to="/" className="profile-link highlight-exit">
+              Sair
+            </Link>
           </div>
         </div>
-      </Main>
-    );
-  }
-}
+      </div>
+    </Main>
+  );
+};
+
+export default Profile;
