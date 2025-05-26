f816bc0b23db697088a9ae62d651bc30471f4aae
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-26 16:49:01 -0300

    feat: hide logo and nav on landingPage

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,16 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 
 import Logo from '../components/template/Logo';
 import Nav from '../components/template/Nav';
 import AppRoutes from './Routes';
-export default props =>
-    <BrowserRouter>
-        <div className="app">
-            <Logo />
-            <Nav />
-            <AppRoutes /> 
+
+function AppContent() {
+    const location = useLocation();
+    const isLandingPage = location.pathname === '/';
+
+    return (
+        <div className={`app ${isLandingPage ? 'landing' : ''}`}>
+            {!isLandingPage && <Logo />}
+            {!isLandingPage && <Nav />}
+            <AppRoutes />
         </div>
-    </BrowserRouter>
+    );
+}
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <AppContent />
+        </BrowserRouter>
+    );
+}
