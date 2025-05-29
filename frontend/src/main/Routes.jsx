8b97924c141332b0238bad1e3c25119749c2f168
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-29 16:49:21 -0300

    feat: protect private routes

--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -7,23 +7,61 @@ import UserCrud from '../components/user/UserCrud';
 import Tasks from '../components/tasks/Tasks';
 import TasksDetails from '../components/tasks/TasksDetails';
 import Board from '../components/leaderboard/Board';
-import UserLogin from '../components/user/UserLogin';
 import Profile from '../components/profile/Profile';
 import MyTasks from '../components/profile/Mytasks';
+import PrivateRoute from './PrivateRoute';
 
 export default function AppRoutes() {
-    return (
-        <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/login" element={<UserLogin />} />
-            <Route path="/users" element={<UserCrud />} />
-            <Route path="/tasks/:id" element={<TasksDetails />} />
-            <Route path="/tasks" element={<Tasks />} />
-            <Route path="/leaderboard" element={<Board />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/mytasks" element={<MyTasks />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/tasks" element={<Tasks />} />
+      <Route path="/board" element={<Board />} />
+
+      <Route
+        path="/home"
+        element={
+            <Home />
+        }
+      />
+
+      <Route
+        path="/tasks/:id"
+        element={
+          <PrivateRoute allowedRoles={['user', 'admin']}>
+            <TasksDetails />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/profile"
+        element={
+          <PrivateRoute allowedRoles={['user', 'admin']}>
+            <Profile />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/mytasks"
+        element={
+          <PrivateRoute allowedRoles={['user', 'admin']}>
+            <MyTasks />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/users"
+        element={
+          <PrivateRoute allowedRoles={['admin']}>
+            <UserCrud />
+          </PrivateRoute>
+        }
+      />
+
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
